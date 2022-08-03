715c8167b67de6df58d3bf2b2ef950b175feee69
Tofulati <almtho2003@gmail.com>
2022-08-02 19:35:09 -0700

    Updated prework HelloWorld App
    Added string change feature and background color change feature.

--- HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
+++ HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,8 +15,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         findViewById<Button>(R.id.button).setOnClickListener {
-            Log.i("Albert", "Tapped on button")
-            findViewById<TextView>(R.id.textView).setTextColor(getResources().getColor(R.color.white))
+            Log.i("Name", "Tapped name")
+            findViewById<TextView>(R.id.textView).setTextColor(getResources().getColor(R.color.black))
+        }
+
+        findViewById<Button>(R.id.button2).setOnClickListener {
+            Log.i("Back", "Tapped back")
+            findViewById<ConstraintLayout>(R.id.backgroundView).setBackgroundColor(getResources().getColor(R.color.white))
+        }
+
+        findViewById<Button>(R.id.button3).setOnClickListener {
+            Log.i("String", "Tapped string")
+            val editText = findViewById<EditText>(R.id.editText)
+            val text = editText.text
+            findViewById<TextView>(R.id.textView).setText(text.toString())
         }
     }
+
 }
